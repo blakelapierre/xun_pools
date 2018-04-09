b8c4ea683491d6d86b5b6fbc7f876ee5fd3c1d41
Blake La Pierre <blakelapierre@gmail.com>
2018-04-09 12:09:11 -0400

    more pools

--- www/src/pools.ts
+++ www/src/pools.ts
@@ -2,5 +2,7 @@ export default [
   'http://alpha.ultranote.org:8117/live_stats',
   'http://beta.ultranote.org:8117/live_stats',
   'http://delta.ultranote.org:8117/live_stats',
-  'https://cryptoknight.cc/rpc/xun/live_stats'
+  'https://cryptoknight.cc/rpc/xun/live_stats',
+  'http://ru.ultrapool.info:8117/live_stats',
+  'http://xun.corpopool.com/api/live_stats'
 ];
